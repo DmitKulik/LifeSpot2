71962ab41f76600e40002b7fb6bc66c8801faf3a
Dmitry <grammattika@gmail.com>
2023-10-28 15:15:46 +0300

    Добавьте файлы проекта.

---
+++ LifeSpot/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using static LifeSpot.Logger;
+
+namespace LifeSpot{
+    public class Program{
+        public static void Main(string[] args){
+
+            PrintMessage( (() => Info("Запуск приложения")) );
+            
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
